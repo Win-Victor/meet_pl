75fddb9e780f254a57352b1a049f1eaf7d4ae5e8
“win-victor” <win-victor@mail.ru>
2023-01-25 11:27:07 +0300

    tasks 25, 27 is completed

--- hw4/Program.cs
+++ hw4/Program.cs
@@ -5,6 +5,19 @@
 2, 4 -> 16
 */
 
+void MyPow(int a, int b)
+{
+    int res = a;
+    for (int count = 1; count < b; count++) res *= a;
+    System.Console.WriteLine($"Возведение числа {a} в степень {b}: {res}.");
+}
+
+MyPow(3, 5);
+MyPow(2, 4);
+MyPow(2, 10);
+
+System.Console.WriteLine();
+
 /*
 Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 452 -> 11
@@ -12,10 +25,24 @@
 9012 -> 12
 */
 
+
+void SumNumbers()
+{
+    System.Console.Write("Введите натуральное число: ");
+    string num = System.Console.ReadLine();
+    int res = 0;
+    for (int count = 0; count < num.Length; count++) res += Convert.ToInt32(Convert.ToString(num[count]));
+    System.Console.WriteLine($"Сумма цифр в введенном числе {num}: {res}");
+}
+
+SumNumbers();
+
+System.Console.WriteLine();
+
 /*
 Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
 */
 
-System.Console.WriteLine(Math.Pow(2, 4));
+int [] 
