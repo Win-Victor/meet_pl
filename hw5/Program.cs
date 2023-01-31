4f965537b846fded7c547a0b92be50cc5b22101a
“win-victor” <win-victor@mail.ru>
2023-01-31 13:33:52 +0300

    added info

--- hw5/Program.cs
+++ hw5/Program.cs
@@ -2,6 +2,20 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+int [] arr34 = FillArrayWithRandomNumbers(10, 100, 999);
+
+// public void CountEvenAndOddNumbers(int [] array, int out Even )
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. 
 // Найдите сумму элементов, стоящих на нечётных позициях.
