c58b0c5f3e0a363a2ed42f49fcde9a211049dddd
“win-victor” <win-victor@mail.ru>
2023-02-02 14:29:45 +0300

    hw6 completed

--- hw6/Program.cs
+++ hw6/Program.cs
@@ -12,15 +12,46 @@ while (i <= m)
 {
     System.Console.Write($"Введите {i}-e число: ");
     number = Convert.ToInt32(Console.ReadLine());
-    if(number > 0) count++;
+    if (number > 0) count++;
     i++;
 }
 System.Console.WriteLine($"Было введено чисел: {count}, больших 0.");
-// До просмотра семинара
-
 
 
 // Задача 43: Напишите программу, которая найдёт точку пересечения 
 // двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
 // значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+/* 
+y = k1 * x + b1 
+y = k2 * x + b2
+
+k1x + b1 = k2x + b2
+k1x - k2x = b2 - b1
+(k1 - k2) * x = b2 - b1
+x = (b2 - b1) / (k1 - k2) 
+*/
+
+System.Console.WriteLine("Введите все переменные. k1 и k2 не должны равняться.");
+
+var p = new Dictionary<string, double>()
+{
+    {"b1", 0},
+    {"k1", 0},
+    {"b2", 0},
+    {"k2", 0}
+};
+
+foreach (var point in p)
+{
+    Console.Write($"Введите {point.Key}: ");
+    p[point.Key] = Convert.ToInt32(Console.ReadLine());
+}
+if (p["k1"] == p["k2"]) System.Console.WriteLine("k1 == k2. Данная система не имеет решения.");
+else
+{
+    double x = (p["b2"] - p["b1"]) / (p["k1"] - p["k2"]);
+    double y = p["k1"] * ((p["b2"] - p["b1"]) / (p["k1"] - p["k2"])) + p["b1"];
+    System.Console.Write($"({Math.Round(x, 3)}; {Math.Round(y, 3)})");
+}
