b9e4d93131ee05cd23fd05ede881e155e6011653
“win-victor” <win-victor@mail.ru>
2023-02-04 01:15:09 +0300

    modified task47 more

--- hw7/Program.cs
+++ hw7/Program.cs
@@ -5,13 +5,14 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+double[,] GetFillPrintArray()
+{
 Random rand = new Random();
 System.Console.Write("Для создания массива введите количество строк(m) и количество столбцов(n).\nСтрок:");
 int m = Convert.ToInt32(System.Console.ReadLine());
 System.Console.Write("Столбцов:");
 int n = Convert.ToInt32(System.Console.ReadLine());
 double[,] array2d = new double[m, n];
-
 for(int i = 0; i < array2d.GetLength(0); i++)
 {
     for(int j = 0; j < array2d.GetLength(1); j++) 
@@ -20,14 +21,11 @@ for(int i = 0; i < array2d.GetLength(0); i++)
         System.Console.Write($"{Math.Round(array2d[i,j], 2)}\t");
     }
     System.Console.WriteLine();
-} 
+}
+return array2d;
+}
 
-
-// for(int i = 0; i < array2d.GetLength(0); i++)
-// {
-//     for(int j = 0; j < array2d.GetLength(1); j++) System.Console.Write($"{Math.Round(array2d[i,j], 2)}\t");
-//         System.Console.WriteLine();
-// } 
+double[,] myArr = GetFillPrintArray();
 
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
 // и возвращает значение этого элемента или же указание, что такого элемента нет.
@@ -37,8 +35,9 @@ for(int i = 0; i < array2d.GetLength(0); i++)
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
-// int[,] array = new int[4,3]
-// {1, 4, 7, 2};
+double[,] array50 = GetFillPrintArray();
+
+
 
 
 
